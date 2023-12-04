5a107ecab1bfa0a0a0618d48559c721ab7de5c56
JackStendeback <jackstendeback@gmail.com>
2023-12-04 14:56:03 -0600

    created a functional button to add table data

---
+++ script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const addTask = document.getElementById('task-button');
+    const taskTable = document.getElementById('task-table').getElementsByTagName('tbody')[0];
+
+    addTask.addEventListener('click', function () {
+        // Creating a new table row upon clicking.
+        const newRow = document.createElement('tr');
+
+        // Creating two table data cells for each added row.
+        const taskCell = document.createElement('td');
+        const checkboxCell = document.createElement('td');
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.classList.add('inputs');
+
+        // Adding content to the table data cells
+        taskCell.textContent = 'New Task';
+        checkboxCell.appendChild(checkbox);
+
+        // Append the cells to the new row
+        newRow.appendChild(taskCell);
+        newRow.appendChild(checkboxCell);
+
+        // Finally, append the new row to the table body
+        taskTable.appendChild(newRow);
+    });
+})
