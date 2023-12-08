825cae8fea061c13025390dced67358c8b54c6a9
JackStendeback <jackstendeback@gmail.com>
2023-12-07 20:10:03 -0600

    Adding javascript baseline functions

---
+++ poetry-generator/main.js
@@ -0,0 +1,14 @@
+document.getElementById('generate-btn').addEventListener('click', generatePoem);
+document.getElementById('save-btn').addEventListener('click', savePoem);
+
+function generatePoem() {
+
+}
+
+function savePoem() {
+
+}
+
+window.onload = function() {
+    
+}
